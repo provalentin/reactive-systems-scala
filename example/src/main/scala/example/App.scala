7bc1877af5d5b59fb5f378ebccccacf35445e660
provalentin <provalentin@gmail.com>
2019-03-30 00:00:27 +0200

    Update App.scala

--- example/src/main/scala/example/App.scala
+++ example/src/main/scala/example/App.scala
@@ -23,26 +23,36 @@ object App {
   }
 
   //Find the Kth element of a list.
-  def kEl(n: Int, ls: List[Integer]): Int = (n, ls) match {
+  def kEl(n: Int, ls: List[Int]): Int = (n, ls) match {
     case (0, h::tail) => h
     case(k, h::tail) => kEl(k-1,tail)
     case(_, Nil) => throw new NoSuchElementException
   }
 
   //P04 (*) Find the number of elements of a list.
-  def sizeR(l: List[Integer]):Int = l match {
+  def sizeR(l: List[Int]):Int = l match {
     case (Nil) => 0
     case (h::tail) =>1 + sizeR(tail)
   }
 
-  def sizeTR(l: List[Integer]):Int = {
-    def size(n:Int, l: List[Integer]):Int = (n,l) match {
+  def sizeTR(l: List[Int]):Int = {
+    def size(n:Int, l: List[Int]):Int = (n,l) match {
         case (p, Nil) => 0
         case (k, h::tail) => size(k+1, tail)
       }
     size(0,l)
   }
 
-  def sizeNative(l: List[Integer]):Int = l.length
-
+  def sizeNative(l: List[Int]):Int = l.length
+  
+  def sizeF(l: List[Int]): Int = ???
+  
+  def reverse(l: List[Int]): List[Int] = ???
+  
+  def reverseR(l: List[Int]): List[Int] = ???
+  
+  def reverseTR(l: List[Int]): List[Int] = ???
+  
+  def reverseR(l: List[Int]): List[Int] = ???
+  
 }
